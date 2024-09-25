18a09d3e4153a1400aee5a0b2dfcff08e7004e26
mihai.tamazlicaru <mihai.tamazlicaru@dsi.md>
2024-09-25 10:46:26 +0300

    Add appoinments views

---
+++ ProgramareAC.Web/Controllers/PriorAppointmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramareAC.Web.Controllers
+{
+    public class PriorAppointmentController : Controller
+    {
+        // GET: PriorAppointment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
