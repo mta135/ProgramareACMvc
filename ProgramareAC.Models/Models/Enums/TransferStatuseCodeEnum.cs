5e6465e0bf8bbe0d2d1a6515731d3a0bd67b7a28
mihai.tamazlicaru <mihai.tamazlicaru@dsi.md>
2024-11-03 13:30:54 +0200

    Add Transfer Statuse code Class

---
+++ ProgramareAC.Models/Models/Enums/TransferStatuseCodeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramareAC.Models.Models.Enums
+{
+    public class TransferStatuseCodeEnum
+    {
+        public static int MsignSucces = 0;
+        
+        public static int OracleTransferError = 1;
+        
+        public static int MsignError = 2;
+        
+        public static int MpassError = 3;
+    }
+}
